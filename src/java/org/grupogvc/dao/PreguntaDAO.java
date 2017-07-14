3a4012ca341cfc880457abe68b66d3e7fd961d86
Jerusalen <Jerusalen@Jerusalen-PC>
2017-07-14 03:42:18 -0500

    Merge origin/master
    Conflicts:
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties

---
+++ src/java/org/grupogvc/dao/PreguntaDAO.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.grupogvc.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import org.grupogvc.conexion.Conexion;
+import org.grupogvc.modelo.Categoria;
+import org.grupogvc.modelo.Pregunta;
+
+/**
+ *
+ * @author Jerusalen
+ */
+public class PreguntaDAO extends Conexion{
+    public void registrarPregunta(Pregunta preguntaregistra) throws Exception{
+        try{
+            this.Conectar();
+            PreparedStatement consulta= this.getConexion().prepareStatement("INSERT INTO pregunta (pregunta,idcategoria) values(?,?)");
+          
+             consulta.setString(1, preguntaregistra.getPregunta());
+            consulta.setInt(2,preguntaregistra.getCategoria().getIdCategoria());
+           
+            consulta.executeUpdate();
+        }
+        catch(Exception e){
+        System.out.println("error en Pregunta DAO -->Registrar Pregunta"+"/n");
+        throw e;
+       // System.out.println("error en DAO"); 
+        }
+     finally{
+           this.Cerrar();
+        }
+    }
+     
+     public List<Pregunta> listarPregunta() throws Exception{//
+     List<Pregunta> lista;
+        ResultSet resultadoset;
+     try{
+         this.Conectar();
+         PreparedStatement consulta=this.getConexion().prepareCall("SELECT * FROM pregunta");
+         resultadoset= consulta.executeQuery();
+         lista =new ArrayList();
+         while(resultadoset.next()){
+             Pregunta pregunta=new Pregunta();
+             pregunta.setIdpregunta(resultadoset.getInt("idpregunta"));
+             pregunta.setPregunta(resultadoset.getString("pregunta"));
+             pregunta.setCategoria(new CategoriaDAO().buscarIdCategoria((resultadoset.getInt("idcategoria"))));
+             
+             lista.add(pregunta);
+         }
+     }
+     catch(Exception e){
+          System.out.println("error en Pregunta DAO -->Listar Pregunta"+"/n");
+          throw e;
+     }
+     finally{
+         this.Cerrar();
+     }
+     return lista;
+    }
+     
+     public Pregunta elegirDatoPregunta(Pregunta pregunta) throws Exception{
+        Pregunta preguntados=null;
+        ResultSet resultadoset;
+        
+        try{
+            this.Conectar();
+             PreparedStatement consulta= this.getConexion().prepareStatement("SELECT * FROM pregunta WHERE idpregunta=?");
+            consulta.setInt(1, pregunta.getIdpregunta());
+            resultadoset = consulta.executeQuery();
+            while(resultadoset.next())
+            {
+              preguntados= new Pregunta();
+              preguntados.setIdpregunta(resultadoset.getInt("idpregunta"));
+              preguntados.setPregunta(resultadoset.getString("pregunta"));
+              preguntados.setCategoria(new CategoriaDAO().buscarIdCategoria((resultadoset.getInt("idcategoria"))));
+             
+            }
+        }
+        catch(Exception e){
+            System.out.println("error en Pregunta DAO -->Elegir Pregunta"+"/n");
+            throw e;
+        }
+        finally{
+           this.Cerrar();
+        }
+        return preguntados;
+    }
+     
+     public void modificarPregunta (Pregunta preguntamodificar) throws Exception{
+        try{
+            this.Conectar();
+            PreparedStatement consulta= this.getConexion().prepareStatement("UPDATE pregunta SET pregunta=?,idcategoria=? WHERE idpregunta=?");
+            consulta.setString(1, preguntamodificar.getPregunta());
+            consulta.setInt(2,preguntamodificar.getCategoria().getIdCategoria());
+            consulta.setInt(3,preguntamodificar.getIdpregunta());
+            
+            consulta.executeUpdate();
+        }
+        catch(Exception e){
+            System.out.println("Error en Modificar Pregunta DAO");
+           throw e; 
+        }
+        finally{
+           this.Cerrar();
+        }
+    }
+     
+     public Pregunta buscarIdPregunta(int idpregunta) throws Exception{
+        Pregunta preguntabusca= new Pregunta();
+        ResultSet resultadosetbusca;
+      try{
+            this.Conectar();
+            PreparedStatement consulta= this.getConexion().prepareCall("SELECT * FROM pregunta WHERE idpregunta=?");
+            consulta.setInt(1,idpregunta);
+            resultadosetbusca=consulta.executeQuery();
+            if(resultadosetbusca.next()){
+            preguntabusca.setIdpregunta(resultadosetbusca.getInt("idpregunta"));
+            preguntabusca.setPregunta(resultadosetbusca.getString("pregunta"));
+            preguntabusca.setCategoria(new CategoriaDAO().buscarIdCategoria((resultadosetbusca.getInt("idcategoria"))));
+            }
+            resultadosetbusca.close();
+            
+        }
+        catch(Exception e){
+            System.out.println("error en PreguntaDAO->buscarIdPregunta "+e);
+           throw e; 
+        }
+        finally{
+           this.Cerrar();
+        }  
+        return preguntabusca;
+    }
+} 
